b20f644d7c0ba8fbbf1c915050061def9f91ead1
Jay <sultan.jayfahadp@gmail.com>
2026-02-11 18:03:03 +0800

    final final

--- laravel_project/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TableController;
-
-Route::get('/user', [TableController::class, 'getAllStudents']);
-
-Route::apiResource('students', TableController::class);
